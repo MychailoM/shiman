71308562d881553b6caeef83f28893349578639f
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-03-27 18:14:00 +0200

    end of work on 27.03

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,11 @@ const closeMobileMenu = document.querySelector('.close-mobile-menu');
 
 openMobileMenu.addEventListener('click', () => {
     mobileMenu.classList.add('open');
-
+    document.body.style.overflow= 'hidden';
 })
 
 closeMobileMenu.addEventListener('click', () => {
     mobileMenu.classList.remove('open');
-
+    document.body.style.overflowY= 'auto';
 })
 
