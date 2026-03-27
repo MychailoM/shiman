09b9f801861692495970ffb66c453ecdb4bc5468
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-03-27 15:16:22 +0200

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 const openMobileMenu = document.querySelector('.open-mobile-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const backdrop = document.querySelector('.backdrop');
 const closeMobileMenu = document.querySelector('.close-mobile-menu');
 
 openMobileMenu.addEventListener('click', () => {
     mobileMenu.classList.add('open');
-    backdrop.classList.add('open');
+
 })
 
 closeMobileMenu.addEventListener('click', () => {
     mobileMenu.classList.remove('open');
-    backdrop.classList.remove('open');
+
 })
+
