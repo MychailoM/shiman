907e280d5717c1bd210c16ad4d3ed75d285ecdc1
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-03-29 15:34:08 +0300

    section reviews

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,28 @@ closeMobileMenu.addEventListener('click', () => {
     document.body.style.overflowY= 'auto';
 })
 
+
+
+const slides = document.getElementById("slides");
+const totalSlides = slides.children.length;
+const reviewsNum = document.querySelector(".reviews-num");
+let index = 0;
+
+function showSlide(i) {
+  if (i < 0) index = totalSlides - 1;
+  else if (i >= totalSlides) index = 0;
+  else index = i;
+
+  slides.style.transform = `translateX(-${index * 100}%)`;
+  reviewsNum.textContent = `${index + 1}/${totalSlides}`;
+}
+
+reviewsNum.textContent = `1/${totalSlides}`;
+
+document.querySelector(".prevSlide").addEventListener("click", () => {
+  showSlide(index - 1);
+});
+
+document.querySelector(".nextSlide").addEventListener("click", () => {
+  showSlide(index + 1);
+});
