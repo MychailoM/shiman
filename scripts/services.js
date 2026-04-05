7f78f3e6c3e742f47b42967073858bc4fd8823b1
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-04-05 18:18:07 +0300

    work on services

---
+++ scripts/services.js
@@ -0,0 +1,64 @@
+const openMobileMenu = document.querySelector('.open-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeMobileMenu = document.querySelector('.close-mobile-menu');
+const mobileLinks = document.querySelectorAll('.mobile-menu-nav-link');
+
+openMobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.add('open');
+    document.body.classList.add('no-scroll');
+})
+
+closeMobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('open');
+    document.body.classList.remove('no-scroll');
+})
+
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('open');
+    document.body.classList.remove('no-scroll');
+  });
+})
+
+
+const services = document.querySelectorAll('.cleaning-area');
+
+services.forEach((service) => {
+  const serviceToggler = () => {
+    const isActive = service.classList.contains('active');
+
+    services.forEach(i => i.classList.remove('active'));
+
+    if(!isActive){
+      service.classList.add('active');
+    }
+  }
+
+  service.addEventListener('click', serviceToggler)
+})
+
+
+const contactBtn = document.querySelector('.contact-btn');
+const contactBackdrop = document.querySelector('.contact-backdrop');
+const contactClose = document.querySelector('.close-contact-modal');
+
+
+contactBtn.addEventListener("click", () => {
+  contactBackdrop.classList.add('active');
+  document.body.classList.add('no-scroll');
+});
+
+contactClose.addEventListener("click", () => {
+  contactBackdrop.classList.remove('active');
+  document.body.classList.remove('no-scroll');
+});
+
+
+const onTop = document.querySelector('.onTop');
+
+onTop.addEventListener('click', () => {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+})
